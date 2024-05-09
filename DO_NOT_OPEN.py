5dd768c8e7a06d4ee6a435b95b504c9b029f23c8
MMBKMC <151632121+MMBKMC@users.noreply.github.com>
2024-05-09 13:22:47 +0630

    Add files via upload

---
+++ DO_NOT_OPEN.py
@@ -0,0 +1,10 @@
+import sys
+import time
+
+print("Installing Virus----------")
+print("Installing No_escape.exe")
+print("Installing----------")
+time.sleep(4.0)
+
+print("I am just kidding!")
+input("")
